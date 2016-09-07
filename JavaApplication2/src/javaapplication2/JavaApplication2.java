3129163d2e4fd5b8d22e5fc99d3846f95ee32423
briacamp <briacamp@BRIACAMP-US.us.oracle.com>
2016-09-07 10:21:41 -0400

    Commit println fix

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -17,7 +17,7 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         System.out.println("hello java");
-        System.out.println
+        System.out.println("this is fixed");
         // TODO code application logic here
     }
     
